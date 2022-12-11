70fdc26fc0c3bda5e212fbbe1e5c9719123cd767
Lesha321123 <brovinal3@gmail.com>
2022-12-11 18:49:22 +0300

    add choice

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,24 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        switch (input) {
+            case ("1") :
+                System.out.println("Your choice: " + input);
+                Cli.greeting();
+                break;
+            case ("0") :
+                break;
+        }
+
+
+
     }
 }
